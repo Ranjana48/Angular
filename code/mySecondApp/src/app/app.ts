aebb798d48065a396a4bfd04c4aaf3f74d8a23f6
Ranjana Kumari <code0994092@gmail.com>
2025-07-16 09:22:58 +0530

    secondApp commit

--- code/mySecondApp/src/app/app.ts
+++ code/mySecondApp/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Movie } from './movie/movie';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Movie],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('mySecondApp');
